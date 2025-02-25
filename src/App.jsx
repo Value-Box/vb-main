7b90aeb586ffaf8382d141048eca9140c2b43306
Muhammad Ramzan <mr4323992@gmail.com>
2025-02-25 10:14:57 +0500

    apply lazy loading in website

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,49 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./Components/Headers/Navbar";
-
-import Footer from "./Components/Footer";
-import Home from "./Pages/Home";
-import LoginSignup from "./Pages/Login/LoginSignup";
-import ResetPassword from "./Pages/Login/ResetPassword";
-import FindYourAccount from "./Pages/Login/FindYourAccount";
-import GetCode from "./Pages/Login/GetCode";
-
-import LoginNavbar from "./Components/Headers/LoginNavbar";
-import { useState, useEffect } from "react";
+import { useState, useEffect, lazy, Suspense } from "react";
 import { authAPI, getToken } from "./hooks/authAPI";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 
+const Navbar=lazy(()=>import("./Components/Headers/Navbar"))
+const Footer=lazy(()=>import("./Components/Footer"))
+const Home=lazy(()=>import("./Pages/Home"))
+const LoginSignup=lazy(()=>import("./Pages/Login/LoginSignup"))
+const ResetPassword=lazy(()=>import("./Pages/Login/ResetPassword"))
+const FindYourAccount=lazy(()=>import("./Pages/Login/FindYourAccount"))
+const GetCode=lazy(()=>import("./Pages/Login/GetCode"))
+const LoginNavbar=lazy(()=>import("./Components/Headers/LoginNavbar"))
 
 const Layout=()=>{
   const location=useLocation()
   const loginRoutes = ["/LoginSignup", "/ResetPassword", "/FindYourAccount", "/GetCode"];
   const isLogin = loginRoutes.includes(location.pathname); // ✅ Improved check
 
-  useEffect(() => {
-    authAPI(); // ✅ Page Load hone par call hoga
-  }, []);
-  
   return (
-   
       <>
       {isLogin?<LoginNavbar/>:<Navbar />}
-      
-
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/LoginSignup" element={<LoginSignup />} />
-  <Route path="/ResetPassword" element={<ResetPassword/>} />
-  <Route path="/FindYourAccount" element={<FindYourAccount/>} />
-  <Route path="/GetCode" element={<GetCode/>} />
-</Routes>
-{isLogin?'':<Footer />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/LoginSignup" element={<LoginSignup />} />
+          <Route path="/ResetPassword" element={<ResetPassword/>} />
+          <Route path="/FindYourAccount" element={<FindYourAccount/>} />
+          <Route path="/GetCode" element={<GetCode/>} />
+        </Routes>
+        {isLogin?'':<Footer />}
       </>
-    
   );
 }
 
-function App() {
-  return (
-    <Router basename="/vb-main">
-      <Layout />
-    </Router>
-  );
-  
-}
+  function App() {
+    useEffect(() => {
+      authAPI(); // ✅ Page Load hone par call hoga
+    }, []);
+
+    return (
+      <Suspense>
+      <Router basename="/vb-main">
+        <Layout />
+      </Router>
+      </Suspense>
+    );
+    
+  }
 
 export default App;
