7a030a7e442c78ac71372596784a523965d2d45c
Muhammad Ramzan <mr4323992@gmail.com>
2025-02-25 12:58:33 +0500

    minor change in base url

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://182.176.166.222:8081/api/"
+const BASE_URL = window.location.protocol==='https'?
+ "https://cors-anywhere.herokuapp.com/http://182.176.166.222:8081/api/":
+ "http://182.176.166.222:8081/api/"
 
 export const API_ENDPOINTS = {
     GetBrands: `${BASE_URL}/GetBrands/GetBrands`,
