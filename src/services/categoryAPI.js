d55dce9243008483d026db8883ef21483a1591b2
Muhammad Ramzan <mr4323992@gmail.com>
2025-02-25 13:56:14 +0500

    change category component

--- src/services/categoryAPI.js
+++ src/services/categoryAPI.js
@@ -1,12 +1,8 @@
 import { API_ENDPOINTS } from "../config/apiEndpoints";
-import { getToken } from "../hooks/authAPI";
 import useFetch from "../hooks/UseFetch";
 
 const useFetchHomeCategories=()=>{
-    const token=getToken()
-    if(token){
-        return useFetch({url:API_ENDPOINTS.GetCategorys})
-    }   
+    return useFetch({url:API_ENDPOINTS.GetCategorys}) 
 }
 
 export default useFetchHomeCategories;
