0c9aaf204d79e325973d9c15e26c830423f4196c
Muhammad Ramzan <mr4323992@gmail.com>
2025-03-10 13:57:09 +0500

    add check box and quantity add and remove functionality

--- src/Components/CheckBox.jsx
+++ src/Components/CheckBox.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-function CheckBox({ onClick ,id}) {
+function CheckBox({ onClick ,id,checked,onChange}) {
   return (
     <label className="flex items-center space-x-2 cursor-pointer" onClick={onClick}>
       <div className="inline-flex items-center">
         <label className="flex items-center cursor-pointer relative">
           <input 
-            
+            checked={checked}
             type="checkbox" 
             className="peer h-4 w-4 2xl:h-5 2xl:w-5 cursor-pointer transition-all appearance-none rounded-full border border-[#666] checked:bg-[#002882] checked:border-[#002882]" 
-            id={id} 
+            id={id}
+            onChange={onChange ? onChange : () => {}}
           />
           <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 2xl:h-3.5 2xl:w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="1">
