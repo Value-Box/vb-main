63595f54ac50951875f69167d5979194c8c70764
Muhammad Ramzan <mr4323992@gmail.com>
2025-03-12 15:16:04 +0500

    remove localhost url in vit config file

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/vb-main/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://182.176.166.222:8081",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  plugins: [react(),tailwindcss(),],
+  base: '/vb-main/', // Make sure this is correct!
 });
-
-
