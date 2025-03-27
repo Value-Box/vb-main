1157e1e9b909fde44c0ab16b735d8a9c905d44cc
Muhammad Ramzan <mr4323992@gmail.com>
2025-03-27 15:15:30 +0500

    set modal on home page

--- src/Components/FormButton.jsx
+++ src/Components/FormButton.jsx
@@ -3,7 +3,7 @@ function FormButton({ children, onClick, className, type = "button" }) {
     <button
       type={type}
       onClick={onClick}
-      className={`flex items-center justify-center gap-2 px-4 py-2 w-full border rounded-md ${className}`}
+      className={`flex items-center justify-center gap-2 px-4 py-1.5 2xl:py-2 w-full border rounded-md ${className}`}
     >
       {children}
     </button>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,6 +12,8 @@ import Input from '../Components/Input';
 import FormButton from '../Components/FormButton';
 
 function Home() {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
     const images = [
         "https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
         "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
@@ -126,9 +128,14 @@ function Home() {
     </div>
   )}
     {showModal && (
-  <div className="fixed inset-0 flex items-center p-10 justify-center bg-black/70 backdrop-blur-sm z-[9999] " onClick={() => setShowModal(false)}>
-    <div className="bg-[#FCFCFC] p-7 rounded-lg shadow-lg  max-w-full w-[550px] flex flex-col gap-5 relative">
-    {/* Close Button */}
+ <div 
+ className="fixed inset-0 flex items-center p-4 md:p-6 2xl:p-10 justify-center bg-black/70 z-[9999]" 
+ onClick={() => setShowModal(false)} // ❌ Background click closes modal
+>
+ <div 
+   className="bg-[#FCFCFC] sm:p-4 2xl:p-7 rounded-lg shadow-lg max-w-full w-[550px] flex flex-col gap-5 relative"
+   onClick={(e) => e.stopPropagation()} // ✅ Prevent closing on modal content click
+ >
       <button 
         className="absolute -top-4 -right-4 w-10 h-10 flex items-center justify-center rounded-full bg-[#002882] text-white"
         onClick={() => setShowModal(false)} // 🛠️ Corrected Here!
@@ -150,11 +157,11 @@ function Home() {
         </svg>
       </button>
       <div className="w-full flex items-center justify-center">
-        <form className="px-5 md:px-2">
+        <form className=" px-2 sm:px-5 md:px-2">
         <div className='hidden md:block'>
         <h2 className="text-2xl text-center font-bold">Register/Sign in</h2>
         <div className="py-2 text-center flex items-center justify-center gap-1">
-  <div className="w-4 h-4 flex items-center justify-center bg-[#12B76A] rounded-full border border-[#12B76A]">
+  <div className="2xl:w-4 2xl:h-4 flex items-center justify-center bg-[#12B76A] rounded-full border border-[#12B76A]">
     <svg
       className="w-3 h-3 text-white"
       fill="none"
@@ -166,7 +173,7 @@ function Home() {
       <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
     </svg>
   </div>
-  <span className='text-[16px]'>Your Information is Protected</span>
+  <span className='text-sm 2xl:text-[16px]'>Your Information is Protected</span>
 </div>
          
         </div>
@@ -174,12 +181,12 @@ function Home() {
           {!showPassword ? (
               <div className="mb-4">
                 <label className="block text-blue-900">Email</label>
-                <Input type="text" placeholder="Enter your email" />
+                <Input type="text" value={email} placeholder="Enter your email" onChange={(e)=>setEmail(e.target.value)} />
               </div>
             ) : (
               <div className="mb-4">
                 <label className="block text-blue-900">Password</label>
-                <Input type="password" placeholder="Enter your password" />
+                <Input type="password" value={password} placeholder="Enter your password" onChange={(e)=>setPassword(e.target.value)} />
               </div>
             )}
 
@@ -193,34 +200,34 @@ function Home() {
               Continue
             </FormButton>
             <p 
-              className="mt-2 text-center  cursor-pointer" 
+              className="mt-2 text-center text-sm 2xl:text-base cursor-pointer" 
               onClick={() => setIsModalOpen(true)}
             >
               Trouble Signing in?
             </p>
           <div className="flex items-center w-full mt-2">
       <div className="flex-1 h-px bg-gray-300"></div>
-      <span className="px-3 text-gray-500 text-1xl">Or continue with</span>
+      <span className="px-3 text-gray-500 text-lg 2xl:text-xl">Or continue with</span>
        <div className="flex-1 h-px bg-gray-300"></div>
        </div>
-       <div className='flex flex-col gap-2 mt-2'>
-       <FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full py-2 px-4 w-full mb-1 cursor-pointer">
-       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 36 36" fill="none">
+       <div className='flex flex-col gap-1 mt-2'>
+       <FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full 2xl:py-2 px-4 w-full mb-1 cursor-pointer">
+       <svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-6' viewBox="0 0 36 36" fill="none">
   <path d="M9.93 16.185C12.09 20.43 15.57 23.91 19.815 26.07L23.115 22.77C23.535 22.35 24.12 22.23 24.645 22.395C26.325 22.95 28.125 23.25 30 23.25C30.3978 23.25 30.7794 23.408 31.0607 23.6893C31.342 23.9706 31.5 24.3522 31.5 24.75V30C31.5 30.3978 31.342 30.7794 31.0607 31.0607C30.7794 31.342 30.3978 31.5 30 31.5C23.237 31.5 16.751 28.8134 11.9688 24.0312C7.1866 19.249 4.5 12.763 4.5 6C4.5 5.60218 4.65804 5.22064 4.93934 4.93934C5.22064 4.65804 5.60218 4.5 6 4.5H11.25C11.6478 4.5 12.0294 4.65804 12.3107 4.93934C12.592 5.22064 12.75 5.60218 12.75 6C12.75 7.875 13.05 9.675 13.605 11.355C13.77 11.88 13.65 12.465 13.23 12.885L9.93 16.185Z" fill="#1A1A1A"/>
 </svg>
-  <span className="text-center flex-1">Phone</span>
+  <span className="text-center text-sm 2xl:text-base flex-1">Phone</span>
 </FormButton>
-       <FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full py-2 px-4 w-full mb-1 cursor-pointer">
-  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 36 36" fill="none">
+       <FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full 2xl:py-2 px-4 w-full mb-1 cursor-pointer">
+  <svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-6' viewBox="0 0 36 36" fill="none">
     <path d="M32.7083 15.0623H31.5V15H18V21H26.4773C25.2405 24.4928 21.9172 27 18 27C13.0297 27 9 22.9702 9 18C9 13.0297 13.0297 9 18 9C20.2943 9 22.3815 9.8655 23.9708 11.2792L28.2135 7.0365C25.5345 4.53975 21.951 3 18 3C9.71625 3 3 9.71625 3 18C3 26.2838 9.71625 33 18 33C26.2838 33 33 26.2838 33 18C33 16.9943 32.8965 16.0125 32.7083 15.0623Z" fill="#FFC107"/>
     <path d="M4.72656 11.0183L9.65481 14.6325C10.9883 11.331 14.2178 9 17.9971 9C20.2913 9 22.3786 9.8655 23.9678 11.2793L28.2106 7.0365C25.5316 4.53975 21.9481 3 17.9971 3C12.2356 3 7.23906 6.25275 4.72656 11.0183Z" fill="#FF3D00"/>
     <path d="M17.9983 32.9999C21.8728 32.9999 25.3933 31.5172 28.055 29.1059L23.4125 25.1774C21.8562 26.3617 19.954 27.002 17.9983 26.9999C14.0968 26.9999 10.784 24.5122 9.53603 21.0404L4.64453 24.8092C7.12703 29.6669 12.1685 32.9999 17.9983 32.9999Z" fill="#4CAF50"/>
     <path d="M32.7083 15.0623H31.5V15H18V21H26.4773C25.8857 22.6623 24.82 24.1149 23.412 25.1782L23.4142 25.1768L28.0568 29.1052C27.7283 29.4037 33 25.5 33 18C33 16.9943 32.8965 16.0125 32.7083 15.0623Z" fill="#1976D2"/>
   </svg>
-  <span className="text-center flex-1">Google</span>
+  <span className="text-center text-sm 2xl:text-base flex-1">Google</span>
 </FormButton>
-<FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full py-2 px-4 w-full mb-1 cursor-pointer">
-<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 36 36" fill="none">
+<FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full 2xl:py-2 px-4 w-full mb-1 cursor-pointer">
+<svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-6' viewBox="0 0 36 36" fill="none">
   <g clip-path="url(#clip0_11160_31316)">
     <path d="M36 18C36 8.05894 27.9411 0 18 0C8.05894 0 0 8.05894 0 18C0 26.9842 6.58237 34.431 15.1875 35.7813V23.2031H10.6172V18H15.1875V14.0344C15.1875 9.52312 17.8748 7.03125 21.9864 7.03125C23.9558 7.03125 26.0156 7.38281 26.0156 7.38281V11.8125H23.7459C21.5099 11.8125 20.8125 13.2 20.8125 14.6236V18H25.8047L25.0066 23.2031H20.8125V35.7813C29.4176 34.431 36 26.9844 36 18Z" fill="#1877F2"/>
     <path d="M25.0066 23.2031L25.8047 18H20.8125V14.6236C20.8125 13.1999 21.5099 11.8125 23.7459 11.8125H26.0156V7.38281C26.0156 7.38281 23.9558 7.03125 21.9863 7.03125C17.8748 7.03125 15.1875 9.52313 15.1875 14.0344V18H10.6172V23.2031H15.1875V35.7813C16.1179 35.9271 17.0582 36.0002 18 36C18.9418 36.0002 19.8821 35.9271 20.8125 35.7813V23.2031H25.0066Z" fill="white"/>
@@ -231,17 +238,17 @@ function Home() {
     </clipPath>
   </defs>
 </svg>
-  <span className="text-center flex-1">Facebook</span>
+  <span className="text-center text-sm 2xl:text-base flex-1">Facebook</span>
 </FormButton>
-<FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full py-2 px-4 w-full mb-1 cursor-pointer">
-<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+<FormButton className="bg-white font-semibold text-black border border-gray-300 !rounded-full 2xl:py-2 px-4 w-full mb-1 cursor-pointer">
+<svg xmlns="http://www.w3.org/2000/svg" className='w-5 sm:w-6' viewBox="0 0 36 36" fill="none">
   <path d="M25.5728 30.42C24.1028 31.845 22.4978 31.62 20.9528 30.945C19.3178 30.255 17.8178 30.225 16.0928 30.945C13.9328 31.875 12.7928 31.605 11.5028 30.42C4.1828 22.875 5.26281 11.385 13.5728 10.965C15.5978 11.07 17.0078 12.075 18.1928 12.165C19.9628 11.805 21.6578 10.77 23.5478 10.905C25.8128 11.085 27.5228 11.985 28.6478 13.605C23.9678 16.41 25.0778 22.575 29.3678 24.3C28.5128 26.55 27.4028 28.785 25.5578 30.435L25.5728 30.42ZM18.0428 10.875C17.8178 7.53 20.5328 4.77 23.6528 4.5C24.0878 8.37 20.1428 11.25 18.0428 10.875Z" fill="black"/>
 </svg>
-  <span className="text-center flex-1">Apple</span>
+  <span className="text-center text-sm 2xl:text-base flex-1">Apple</span>
 </FormButton>
        </div>
      
-       <p className="text-center mt-3 text-gray-500">
+       <p className="text-center mt-3 text-gray-500 text-sm 2xl:text-base">
   By continuing, you confirm that you‘re an adult and you’ve read and accepted our   
   <a href="#" className="underline text-gray mx-1">AliExpress</a>
   <a href="#" className="underline text-gray mx-1">Membership Agreement</a> and 
